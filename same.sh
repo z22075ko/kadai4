6bbe5caaf49e4eb84f47e627c450ff3d75a1c3f2
kogawa <z22075ko@aiit.ac.jp>
2022-07-23 14:02:47 +0000

    shell script update

--- same.sh
+++ same.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-#引数の判定
-#失敗時：input 2 argments
-#数値かどうか判定
-#失敗時：input natural number
-#動作開始
-#同じ場合：$1 is equal to $2
-#違う場合：$2 is not equal to $2
+if [ $# -ne 2]; then	#引数の判定
+	echo "input 2 argments" 1>&2
+	exit 1
+fi
+expr $1 + $2 > /dev/null 2>&1
+if [ $? -ge 2 ]; then
+	echo "input natural number" 1>&2
+	exit 1
+fi
+
+if [ $1 -eq $2 ]; then
+	echo "$1 is equal to $2"
+else
+	echo "$1 is not equal to $2"
+fi
+
 
 
